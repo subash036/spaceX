8c8e6b0eaee05830ba2ea964606c871bbdbd8baf
Shiva <e10854@excelityglobal.com>
2020-11-15 16:33:07 +0530

    gride changes

--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,9 @@ export class DashboardService {
   getLaunchList(filterObject:object){
     console.log(`SHIVA: DashboardService -> getLaunchList -> filterObject`, filterObject);
     let queryString = Object.keys(filterObject).map(key => filterObject[key]!==null?key + '=' + filterObject[key]:null).filter(data=>data).join('&');
-    return this.http.get<any>(`${environment.baseUrl}launches?${queryString}`);
+    return this.http.get<any>(`${environment.baseUrl}launches?${queryString}`).pipe(map((data)=>{
+      // data['land_success'] = data?.rocket?.first_stage?.cores;
+      return data;
+    }));
   }
 }
